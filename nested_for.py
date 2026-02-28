c7a2152f800df6673e740be2d8c47c50b4add7e7
Revel <revelcahyadi2004@gmail.com>
2026-02-28 18:12:48 +0700

    feat : nested for loop imp

---
+++ nested_for.py
@@ -0,0 +1,55 @@
+for baris in range(3):
+    for kolom in range(3):
+        print("*", end="")
+    print()
+    
+# baris 0, kolom 0 
+# *
+
+# baris 0, kolom 1
+# **
+
+# baris 0, kolom 2
+# ***
+
+# print()
+# ***
+# 
+
+# baris 1, kolom 0
+# ***
+# *
+
+# baris 1, kolom 1
+# ***
+# **
+
+# baris 1, kolom 2
+# ***
+# ***
+
+# print()
+# ***
+# ***
+# 
+
+# baris 2, kolom 0
+# ***
+# ***
+# *
+
+# baris 2, kolom 1
+# ***
+# ***
+# **
+
+# baris 2, kolom 2
+# ***
+# ***
+# ***
+
+# print()
+# ***
+# ***
+# ***
+# 
